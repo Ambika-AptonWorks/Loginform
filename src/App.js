09299861cefcece8d97fa3298bf45e39c369bc52
Ambika <amika@aptonworks.com>
2022-03-21 21:14:25 +0530

    form

--- src/App.js
+++ src/App.js
@@ -2,82 +2,71 @@ import React,{useState,useEffect} from 'react';
 import './App.css';
 
 function App() {
-  const initialization={username:'',email:'',password:''};
-  const [form,setForm]= useState({initialization});
-  const [formErrors,setFormErrors]= useState({});
-  const[isSubmit, setIsSubmit]=useState(false);
+  const[name,setName]=useState({username:'',email:'',password:''});
+  const[formerrors,setFormErrors]=useState({});
+  const[isSubmit,setIsSubmit]=useState(false);
 
-  const handleChange=(e) =>{
-    console.log(e.target);
-    const {name, value}=e.target;
-    setForm({ ...form, [name]:value});
-    console.log(form);
-    
-  };
-  const handleSubmit=(e) =>{
+  const handleSubmit=(e)=>{
     e.preventDefault();
-    setFormErrors(validate(form));
+    setFormErrors(validate(name));
     setIsSubmit(true);
-
   };
-  useEffect(() =>{
-    console.log(formErrors);
-    if(Object.keys(formErrors).length ===0 && isSubmit)
-    {
-      console.log(form)
+  useEffect(()=>{
+    console.log(formerrors);
+    if(Object.keys(formerrors).length === 0 && isSubmit){
+      console.log(name);
     }
-  },[formErrors]);
+  }, [formerrors]);
   const validate=(values)=>{
     const errors={};
-    const regex=/^[^\s@]+@[^\s@]+\.[^\s@]{2, }$/i;
+    const regex =/^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
     if(!values.username){
-      errors.username="username is required";
+      errors.username="Username is required";
     }
     if(!values.email){
       errors.email="Email is required";
-    }
-    else if(!regex.test(values.email)){
-      errors.email="This ias not valid Email";
+    }else if(!regex.test(values.email)){
+      errors.email="This is not valid mail";
     }
     if(!values.password){
       errors.password="password is required";
-    }else if(values.Password.length<4){
-      errors.Password="password must be more than 4 characters";
-    }else if(values.Password.length>10){
-      errors.Password="password cannot exceed 10 characters";
+    }else if(values.password.length<4){
+      errors.password="password must be greater than 4 characters";
+    }else if(values.password.length>10){
+      errors.password="password cannot exceed 10 characters";
     }
     return errors;
   };
-  return (
-        <div className='container'>
-          <pre>{JSON.stringify(form,undefined,2)}</pre>
-          <form onSubmit={handleSubmit} >
-            <h1>Loginform</h1>
-               <div className='divider'>
-                 <div className='form'>
-                  <div className='field'>
-                    <label>Username</label>
-                    <input 
-                    type='text'
-                    name='username' value={form.username}
-                    onChange={handleChange}></input>
-                  </div>
-                  <div className='field'>
-                    <label>Email</label>
-                    <input type='text' name='email' value={form.email} 
-                    onChange={handleChange}></input>
-                  </div>
-                  <div className='field'>
-                    <label>Password</label>
-                    <input type='password' name='password'value={form.Password}
-                    onChange={handleChange}></input>
-                  </div>
-                  <button type="button" className="btn btn-primary">Submit</button>
-                </div>
-              </div>
-            </form>
-         </div>
+  return(
+    <div className='container'>
+      <h1>Login Form</h1>
+      <form onSubmit={handleSubmit}>
+        <div className='row'>
+          <label>Username</label>
+          <input type="text" name="username" placeholder='username'
+          value={name.username} onChange={e =>setName({...name,username:e.target.value})}></input>
+        </div>
+        <p>{formerrors.username}</p>
+        <div className='row'>
+          <label>Email</label>
+          <input type="text" name="email" placeholder='abcd@gmail.com'
+          value={name.email} onChange={e =>setName({...name,email:e.target.value})}></input>
+        </div>
+        <p>{formerrors.email}</p>
+        <div className='row'>
+          <label>password</label>
+          <input type="password" name="password" placeholder='password'
+          value={name.password} onChange={e =>setName({...name,password:e.target.value})}></input>
+        </div>
+        <p>{formerrors.password}</p>
+        <div className='row' id="btn">
+          <button className='btn btn-primary'>Submit</button>
+          </div>
+      </form>
+      {Object.keys(formerrors).length ===0 && isSubmit ?
+      (<div className='message success'><h2>Signed in successfully!</h2></div>):
+      (<h3>{JSON.stringify(name)}</h3>)}
+    </div>
   );
 }
-
 export default App;
